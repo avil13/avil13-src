6a1afc7d51dd00abb48541e46fa11ba98ea9ed99
avil13 <13.avil@gmail.com>
2019-01-19 02:16:05 +0300

    wip tag

---
+++ components/tag/tag-comp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        TagComponent
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+import { State } from 'vuex-class';
+
+@Component
+export default class TagComponent extends Vue {
+    //
+}
+</script>
